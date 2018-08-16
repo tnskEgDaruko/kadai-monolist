8798d500cbd432ad49598c6c17d33f1d6e2a31a9
Daruko <tnskeg_daruko@yahoo.co.jp>
2018-08-16 01:43:20 +0000

    user login correct

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[user_id] = nil
+    session[:user_id] = nil
     flash[:success] = 'ログアウトしました'
     redirect_to root_url
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_aciton :require_user_logged_in, only: [:show]
+  before_action :require_user_logged_in, only: [:show]
   
   def show
     @user = User.find(params[:id])
