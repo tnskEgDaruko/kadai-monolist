0d1627e835ba8a2ff7d3686275a37d23c74c8d81
Daruko <tnskeg_daruko@yahoo.co.jp>
2018-08-18 05:07:12 +0000

    have ranking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
