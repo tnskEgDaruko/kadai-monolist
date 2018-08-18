4c8222972201f01a2de988dab45a23edea7a93bd
Daruko <tnskeg_daruko@yahoo.co.jp>
2018-08-18 01:40:20 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -10,10 +10,13 @@ class OwnershipsController < ApplicationController
       @item.save
     end
     
-    # Want関係として保存
-    if params[:type] == 'Want'
+    case params[:type]
+    when 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました'
+    when 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました'
     end
     
     redirect_back(fallback_location: root_path)
@@ -24,9 +27,13 @@ class OwnershipsController < ApplicationController
   def destroy
     @item = Item.find(params[:item_id])
     
-    if params[:type] == 'Want'
+    case params[:type]
+    when 'Want'
       current_user.unwant(@item)
       flash[:success] = '商品の Want を解除しました'
+    when 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品の Have を解除しました'
     end
     
     redirect_back(fallback_location: root_path)
